d90a70b90eb3005da40a81f076db76d8463db8e7
2214576751@qq.com <hxl19920401>
2018-11-15 17:18:54 +0800

    去掉greendao，引入google的room数据库框架

---
+++ localstorage/src/main/java/com/justcode/hxl/localstorage/sqlite/Car.java
@@ -0,0 +1,11 @@
+package com.justcode.hxl.localstorage.sqlite;
+
+
+
+
+public class Car {
+
+    private Long id;
+    private String name;
+    private double money;
+}
---
+++ localstorage/src/main/java/com/justcode/hxl/localstorage/sqlite/User.java
@@ -0,0 +1,16 @@
+package com.justcode.hxl.localstorage.sqlite;
+
+
+
+
+public class User {
+
+    private Long id;
+    private int year;
+    private String name;
+
+
+
+
+
+}
